0a8c2b5df2cea09a412959e44019870925973223
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-10-22 22:15:26 -0400

    added balance of

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -14,20 +14,33 @@ import { Base64 } from "./libraries/Base64.sol";
 // We inherit the contract we imported. This means we'll have access
 // to the inherited contract's methods.
 contract MyEpicNFT is ERC721URIStorage {
+
   // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
   using Counters for Counters.Counter;
   //nft id is declared at 0
   Counters.Counter private _tokenIds;
 
+      // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+
+
   // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
   // So, we make a baseSvg variable here that all our NFTs can use.
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  // string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+  // We split the SVG at the part where it asks for the background color.
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   // I create three arrays, each with their own theme of random words.
   // Pick some random funny words, names of anime characters, foods you like, whatever! 
   string[] firstWords = ["NEO", "MATRIX", "MORPHEUS", "METACORTEX", "TRINITY", "BLUE"];
   string[] secondWords = ["RED", "PILL", "WHITE", "RABBIT", "NEBUCHADNEZZAR", "SECRET"];
   string[] thirdWords = ["THE", "ORACLE", "CYPHER", "WIRE", "FU", "RELOADED"];
+
+  // Get fancy with it! Declare a bunch of colors.
+  string[] colors = ["#FFFFFF", "#16db65", "#058c42", "#04471c", "#0d2818", "#020202"];
 
   event NewEpicNFTMinted(address sender, uint256 tokenId);
 
@@ -58,6 +71,21 @@ contract MyEpicNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
+    // Same old stuff, pick a random color.
+  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
+
+  /**
+    * see balance of token owner.
+    */
+  function balanceOf(address owner) public view virtual override returns (uint256) {
+      require(owner != address(0), "ERC721: balance query for the zero address");
+      return _balances[owner];
+  }
+
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
   }
@@ -72,9 +100,13 @@ contract MyEpicNFT is ERC721URIStorage {
     string memory third = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
+    // Add the random color in.
+    string memory randomColor = pickRandomColor(newItemId);
+
+
 
     // I concatenate it all together, and then close the <text> and <svg> tags.
-    string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));    
     console.log("\n--------------------");
     console.log(finalSvg);
     console.log("--------------------\n");
@@ -116,8 +148,7 @@ contract MyEpicNFT is ERC721URIStorage {
     // Increment the counter for when the next NFT is minted.
     _tokenIds.increment();
 
-
-    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     emit NewEpicNFTMinted(msg.sender, newItemId);
+
   }
 }
